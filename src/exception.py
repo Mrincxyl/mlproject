7830bb7058e1f4095ec1d6d2cde192c3ec395f58
MrIncxyl <tsktechnical7@gmail.com>
2026-09-18 16:57:49 +0530

    EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
-import sys, logging
+import sys
+from src.logger import logging
 
 def error__message_detail(error,error_detail:sys):
     
@@ -22,5 +23,3 @@ class CustomException(Exception):
     def __str__(self):
         return  self.error_message     
     
-
-  
